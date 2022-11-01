f160cb02e2e69b24da23786cb6eafe4617368fff
Pedro Neves <pedro.t.dasneves@gmail.com>
2022-10-31 21:11:26 -0300

    Correção

--- funcao17.js
+++ funcao17.js
@@ -1,25 +1,25 @@
 var nome
 var salario
 var aumento
-porcentagemAumento 
-var CalcularNovamente
+var porcentagemAumento
+var calcularNovamente = "s"
 function CadastrarUsuario() {
      nome = prompt("Insira seu nome")
-     salario = prompt("Insira seu salario")
+     salario = parseFloat(prompt("Insira seu salario"))
      if (salario <= 1500) {
           aumento = 1.20
           porcentagemAumento = "20%"
      } else if (salario > 1500 && salario <= 2000) {
           aumento = 1.15
           porcentagemAumento = "15%"
-     } else if (salario > 2000 && <= 3000) {
+     } else if (salario > 2000 && salario <= 3000) {
           aumento = 1.10
           porcentagemAumento = "10%"
      } else {
           aumento = 1.05
           porcentagemAumento = "5%"
      }
-}    
+}
 
 function CalcularAumento(salario, aumento) {
      var salarioAumentado = salario * aumento
@@ -28,15 +28,12 @@ function CalcularAumento(salario, aumento) {
 
 
 function CalcularNovamente() {
-     var CalcularNovamente = prompt("Deseja calcular novamente? s ou n?")
-     return CalcularNovamente
+     calcularNovamente = prompt("Deseja calcular novamente? s ou n?")
 }
 
-while(CalcularNovamente == "s"){
+while (calcularNovamente == "s") {
      CadastrarUsuario()
      CalcularAumento(salario, aumento)
-     console.log("Seu nome é ", nome, " seu salario é ", salario, " você receberá ", porcentagemAumento, " de aumento e seu salário reajustado será de: ", CalcularAumento(salario, aumento) )
+     console.log("Seu nome é " + nome + " seu salario é " + salario + " você receberá " + porcentagemAumento + " de aumento e seu salário reajustado será de: " + CalcularAumento(salario, aumento))
      CalcularNovamente()
 }
-
-
